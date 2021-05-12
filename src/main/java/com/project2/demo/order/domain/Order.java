cad8bddd8899782c38bd1d327175511021cfe030
jhkim <62026666+horobito@users.noreply.github.com>
2021-05-13 01:50:01 +0900

    *User 도메인층 생성

--- src/main/java/com/project2/demo/order/domain/Order.java
+++ src/main/java/com/project2/demo/order/domain/Order.java
@@ -1,4 +1,19 @@
 package com.project2.demo.order.domain;
 
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+
+
+
 }
---
+++ src/main/java/com/project2/demo/user/domain/User.java
@@ -0,0 +1,31 @@
+package com.project2.demo.user.domain;
+
+import com.project2.demo.order.domain.Order;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Embedded
+    public Username username;
+
+    @Embedded
+    public Password password;
+
+
+    private User(Username username, Password password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public static User create(Username username, Password password) {
+        return new User(username, password);
+    }
+}
