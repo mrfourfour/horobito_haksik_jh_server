d234f7883ff058400e2c9dbba72f669f85e01bc1
jhkim <62026666+horobito@users.noreply.github.com>
2021-05-13 23:33:54 +0900

    * 다른 도메인에서 로그인한 유저를 찾을 때 필요한 메소드 임시 생성

---
+++ src/main/java/com/project2/demo/user/service/UserService.java
@@ -0,0 +1,15 @@
+package com.project2.demo.user.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+
+    public UserDto findLoggedUser(){
+        UserDto userDto = null;
+
+        return userDto;
+    }
+}
