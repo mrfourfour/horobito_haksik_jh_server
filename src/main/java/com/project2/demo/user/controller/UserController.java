3b98673605908b20fdb5499a46996cf6eec4e46e
jhkim <62026666+horobito@users.noreply.github.com>
2021-05-18 01:06:41 +0900

    * UserController 따라해보기

--- src/main/java/com/project2/demo/user/controller/UserController.java
+++ src/main/java/com/project2/demo/user/controller/UserController.java
@@ -2,14 +2,48 @@ package com.project2.demo.user.controller;
 
 
 import com.project2.demo.user.service.UserService;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    private final TokenProvider tokenProvider;
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest loginRequest){
+        Token token = userService.login(loginRequest.to());
+        return LoginResponse(token);
+
+    }
+
+
+    @PostMapping("/token/refresh")
+    public Token refreshToken(@RequestBody RefreshTokenPayload refreshTokenPayload){
+        return refresh(refreshTokenPayload);
+    }
+
+    private Token refresh(RefreshTokenPayload refreshTokenPayload){
+        tokenProvider.refresh(refreshTokenPayload.refreshToken);
+    }
+
+    @PostMapping("/sign-up")
+    public LoginResponse signUp(@RequestBody SignUpRequest signUpRequest){
+        Token token = userService.signUp(signUpRequest.to());
+        return LoginResponse(token);
+    }
+
+
+}
+
+class LoginRequest{
+    String username;
+    String password;
+
 
 
 }
