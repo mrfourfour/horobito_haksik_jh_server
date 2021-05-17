a1722b7da07bb3f919e3a4045939e0d7614eed00
jhkim <62026666+horobito@users.noreply.github.com>
2021-05-18 00:41:35 +0900

    * WebSecurityConfig 설정 -> 아주 힘들다

---
+++ src/main/java/com/project2/demo/Menu/controller/MenuController.java
@@ -0,0 +1,12 @@
+package com.project2.demo.Menu.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+
+
+}
