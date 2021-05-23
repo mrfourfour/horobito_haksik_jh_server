f991ba2ae974e277ce04489336256d6ed95a7f37
jhkim <62026666+horobito@users.noreply.github.com>
2021-05-23 22:20:51 +0900

    * Controller 에 menu 생성 부분 추가

--- src/main/java/com/project2/demo/Menu/controller/MenuController.java
+++ src/main/java/com/project2/demo/Menu/controller/MenuController.java
@@ -1,14 +1,27 @@
 package com.project2.demo.Menu.controller;
 
 
+import com.project2.demo.Menu.service.MenuService;
+import com.project2.demo.user.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/menu")
+@AllArgsConstructor
 public class MenuController {
 
+    private final MenuService menuService;
 
+
+    @GetMapping
+    public void createMenu(@RequestBody MenuParameter menuParameter){
+        menuService.createMenu(menuParameter);
+    }
 
 
 }
