5040fad1413107017e88c6859a85607ac5f4609f
jhkim <62026666+horobito@users.noreply.github.com>
2021-05-28 02:31:07 +0900

    * 애그리거트 2개로 분리해서 구현 시도

--- src/main/java/com/project2/demo/interest/domain/PreferenceFoodConverter.java
+++ src/main/java/com/project2/demo/interest/domain/PreferenceFoodConverter.java
@@ -1,4 +1,34 @@
 package com.project2.demo.interest.domain;
 
-public class PreferenceFoodConverter {
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Converter
+public class PreferenceFoodConverter implements AttributeConverter<PreferenceInfo, String> {
+
+
+    @Override
+    public String convertToDatabaseColumn(PreferenceInfo preferenceInfo) {
+        if (preferenceInfo==null){
+            return null;
+        }
+        return preferenceInfo.getGetPreferenceFoodIds()
+                .stream()
+                .map(value -> Long.toString(value))
+                .collect(Collectors.joining(","));
+    }
+
+    @Override
+    public List<Long> convertToEntityAttribute(String dbData) {
+        if (dbData==null){
+            return null;
+        }
+        return Arrays.stream(dbData.split(","))
+                .map(value -> Long.parseLong(value))
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/project2/demo/interest/domain/PreferenceInfo.java
+++ src/main/java/com/project2/demo/interest/domain/PreferenceInfo.java
@@ -1,14 +1,17 @@
 package com.project2.demo.interest.domain;
 
 
+import lombok.Getter;
+
 import javax.persistence.Embeddable;
 import java.util.List;
 
 @Embeddable
+@Getter
 public class PreferenceInfo {
 
 
     private List<String> preferenceTypes;
 
-    private List<String> getPreferenceFoods;
+    private List<Long> getPreferenceFoodIds;
 }
