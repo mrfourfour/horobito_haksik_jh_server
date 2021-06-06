11d13bc97a0fc50ebc03eb9a9dc3614017d40d27
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-06 23:23:46 +0900

    * [OrderService 구현] - [createOrder] 테스트 구현 시작

---
+++ src/test/java/com/project2/demo/order/service/OrderCreateTest.java
@@ -0,0 +1,11 @@
+package com.project2.demo.order.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith()
+class OrderCreateTest {
+
+}
