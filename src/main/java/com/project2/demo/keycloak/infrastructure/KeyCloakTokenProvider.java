6492dbbb5129e099fa290d84c75d5ecb452bcbbc
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-09 00:33:03 +0900

    feat: make [KeyCloakWebClientConfig] class

--- src/main/java/com/project2/demo/keycloak/infrastructure/KeyCloakTokenProvider.java
+++ src/main/java/com/project2/demo/keycloak/infrastructure/KeyCloakTokenProvider.java
@@ -1,9 +1,16 @@
 package com.project2.demo.keycloak.infrastructure;
 
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KeyCloakTokenProvider {
+
+}
+
+@Configuration
+class keyCloakWebClientConfig{
+
 
 }
