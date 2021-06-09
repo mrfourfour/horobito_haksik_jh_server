d9256385d2d95d5db08960d84d5baf45a747da46
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-09 19:31:04 +0900

    feat: create <sessionAuthenticationStrategy> method in [WebSecurityConfig] class

--- src/main/java/com/project2/demo/keycloak/infrastructure/WebSecurityConfig.java
+++ src/main/java/com/project2/demo/keycloak/infrastructure/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
@@ -36,7 +37,7 @@ public class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return null;
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
 
