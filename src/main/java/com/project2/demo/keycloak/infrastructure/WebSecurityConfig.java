7dc1cf21fef60d6f20a43a8d06816b26c3983e80
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-09 18:55:06 +0900

    feat: create <configure> method int [WebSecurityConfig] class

--- src/main/java/com/project2/demo/keycloak/infrastructure/WebSecurityConfig.java
+++ src/main/java/com/project2/demo/keycloak/infrastructure/WebSecurityConfig.java
@@ -5,7 +5,9 @@ import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
@@ -15,8 +17,17 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 // 서버 안에어 security 관련 처리 용도
 public class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests();
+    }
+
     @Override
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return null;
     }
+
+
 }
