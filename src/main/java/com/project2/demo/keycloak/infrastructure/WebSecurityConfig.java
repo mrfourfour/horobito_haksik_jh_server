be8eebadab1f0145597f3f9e67db43723e4971e6
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-09 19:29:58 +0900

    feat: create <configure> method in [WebSecurityConfig] class

--- src/main/java/com/project2/demo/keycloak/infrastructure/WebSecurityConfig.java
+++ src/main/java/com/project2/demo/keycloak/infrastructure/WebSecurityConfig.java
@@ -2,8 +2,10 @@ package com.project2.demo.keycloak.infrastructure;
 
 
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +24,14 @@ public class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests();
+    }
+
+    /*
+    토큰 인증은 직접 구현을 하지 않고,
+    이렇게만 하면 알아서 구현
+     */
+    public void configure(AuthenticationManagerBuilder auth){
+        auth.authenticationProvider(new KeycloakAuthenticationProvider());
     }
 
     @Override
