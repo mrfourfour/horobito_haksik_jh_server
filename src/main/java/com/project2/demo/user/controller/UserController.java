1523a852b5545d28b2bb217244dfb8d7614524a1
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-10 22:55:39 +0900

    fix: fix annotation

--- src/main/java/com/project2/demo/user/controller/UserController.java
+++ src/main/java/com/project2/demo/user/controller/UserController.java
@@ -10,9 +10,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
