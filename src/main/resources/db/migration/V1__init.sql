f4e529b068e2c35fb95524f0e27623a0c54d68e2
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-11 02:05:38 +0900

    chore: give up applying flyway...

--- src/main/resources/db/migration/V1__init.sql
+++
@@ -1,5 +0,0 @@
-drop table if exists account;
-drop sequence if exists hibernate_sequence;
-create sequence hibernate_sequence start with 1 increment by 1;
-create table account (id bigint auto_increment primary key, name varchar(255) null, password varchar(255) null);
-
