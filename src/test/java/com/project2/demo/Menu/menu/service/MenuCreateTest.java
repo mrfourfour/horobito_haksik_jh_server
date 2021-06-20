2c3e7185b624f33c94b409c42e06d84447955e43
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-21 02:44:42 +0900

    test: [createMenu 1- normal] in [MenuCreateTest] class

--- src/test/java/com/project2/demo/Menu/menu/service/MenuCreateTest.java
+++ src/test/java/com/project2/demo/Menu/menu/service/MenuCreateTest.java
@@ -1,39 +1,44 @@
-//package com.project2.demo.Menu.menu.service;
-//
-//
-//import com.project2.demo.Menu.menu.controller.MenuParameter;
-//import com.project2.demo.Menu.menu.domain.MenuRepository;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import static org.mockito.Mockito.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class MenuCreateTest {
-//
-//    @Mock
-//    MenuRepository menuRepository;
-//
-//
-//    @DisplayName("1. 메뉴 만들기")
-//    @Test
-//    void createMenu1(){
-//
-//        MenuService sut
-//                = new MenuService(menuRepository);
-//
-//        MenuParameter menuParameter
-//                = new MenuParameter("pizza"
-//                , 50000,
-//                7,24, 8,30,
-//                0,0,false, "KOREA","NOODLE"
-//                );
-//
-//        sut.createMenu(menuParameter);
-//        verify(menuRepository, times(1)).save(any());
-//
-//    }
-//}
+package com.project2.demo.Menu.menu.service;
+
+
+import com.project2.demo.Menu.menu.controller.MenuParameter;
+import com.project2.demo.Menu.menu.domain.MenuRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MenuCreateTest {
+
+    @Mock
+    MenuRepository menuRepository;
+
+
+    @DisplayName("1. 메뉴 만들기")
+    @Test
+    void createMenu1(){
+
+        MenuService sut
+                = new MenuService(menuRepository);
+
+        MenuParameter menuParameter
+                = new MenuParameter("pizza"
+                , 50000,
+                "this is pizza",
+                LocalTime.parse("08:00:00", DateTimeFormatter.ofPattern("HH:mm:ss")),
+                LocalTime.parse("20:00:00", DateTimeFormatter.ofPattern("HH:mm:ss")),30,
+                "pizza url"
+        );
+
+        sut.createMenu(menuParameter);
+        verify(menuRepository, times(1)).save(any());
+
+    }
+}
