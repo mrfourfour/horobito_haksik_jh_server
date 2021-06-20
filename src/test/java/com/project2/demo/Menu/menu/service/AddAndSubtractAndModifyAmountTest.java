8b964e1eba205b8d0fbd1fe8c2809e2d001effa4
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-21 01:59:39 +0900

    test: [addTest 1- normal] in [AddAndSubtractAndModifyAmountTest] class

--- src/test/java/com/project2/demo/Menu/menu/service/AddAndSubtractAndModifyAmountTest.java
+++ src/test/java/com/project2/demo/Menu/menu/service/AddAndSubtractAndModifyAmountTest.java
@@ -2,6 +2,8 @@ package com.project2.demo.Menu.menu.service;
 
 
 import com.project2.demo.Menu.menu.domain.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -10,6 +12,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AddAndSubtractAndModifyAmountTest {
@@ -31,4 +40,26 @@ public class AddAndSubtractAndModifyAmountTest {
             AmountOfFoodLeft.create(10),
             ImageURL.create("testURL")
     );
+
+    @DisplayName("1. 정상적인 덧셈 ")
+    @Test
+    void addMenu1(){
+
+        MenuService sut
+                = new MenuService(menuRepository);
+
+
+        Long id = Long.parseLong("1");
+
+        int originAmount = menu.getAmountOfFoodLeft().returnFoodLeft();
+        int forAdd = 3;
+
+        when(menuRepository.findMenuByIdAndDeleted(anyLong(), anyBoolean())).thenReturn(menu);
+
+        sut.addAmount(id, forAdd);
+
+        assertEquals(originAmount+forAdd, menu.getAmountOfFoodLeft().returnFoodLeft());
+
+
+    }
 }
