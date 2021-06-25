ea4e521740f0d086cc55e30587a02a2761f42d01
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-25 20:02:34 +0900

    *feat: create [CategorizedFoodRepository] class

---
+++ src/main/java/com/project2/demo/category/categorizedFood/domain/CategorizedFoodRepository.java
@@ -0,0 +1,6 @@
+package com.project2.demo.category.categorizedFood.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategorizedFoodRepository extends JpaRepository<CategorizedFood, Long> {
+}
