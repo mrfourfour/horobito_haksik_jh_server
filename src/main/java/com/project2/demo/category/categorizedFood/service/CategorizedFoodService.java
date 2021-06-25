da91169abaf97cc791fcbff379398fc02c167ccc
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-25 20:19:19 +0900

    *feat: add <addFoodInCategory> method in [CategorizedFoodService] class

---
+++ src/main/java/com/project2/demo/category/categorizedFood/service/CategorizedFoodService.java
@@ -0,0 +1,55 @@
+package com.project2.demo.category.categorizedFood.service;
+
+
+import com.project2.demo.Menu.menu.domain.MenuRepository;
+import com.project2.demo.Menu.menu.service.MenuService;
+import com.project2.demo.category.categorizedFood.domain.CategorizedFood;
+import com.project2.demo.category.categorizedFood.domain.CategorizedFoodRepository;
+import com.project2.demo.category.categorizedFood.domain.CategoryId;
+import com.project2.demo.category.categorizedFood.domain.MenuId;
+import com.project2.demo.category.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CategorizedFoodService {
+
+    private final MenuService menuService;
+    private final MenuRepository menuRepository;
+    private final CategoryService categoryService;
+    private final CategorizedFoodRepository categorizedFoodRepository;
+
+    @Transactional
+    public void addFoodInCategory(Long categoryId, Long menuId) {
+        checkAlreadyExistence(categoryId, menuId);
+        menuService.checkExistence(menuId);
+        categoryService.checkExistence(categoryId);
+        CategorizedFood categorizedFood = CategorizedFood.create(
+                CategoryId.create(categoryId), MenuId.create(menuId)
+        );
+        categorizedFoodRepository.save(categorizedFood);
+    }
+
+    private void checkAlreadyExistence(Long categoryId, Long menuId) {
+        if (categorizedFoodRepository.findByIdAndMenuId(CategoryId.create(categoryId), MenuId.create(menuId))!=null){
+            throw new IllegalArgumentException();
+        }
+    }
+
+//    @Transactional
+//    public void deleteMenuInCategory(Long categoryId, Long menuId) {
+//        Category category = categoryService.getCategoryById(categoryId);
+//        checkExistence(category);
+//        FoodId foodId = FoodId.create(menuId);
+//        checkExistence(category, foodId);
+//        category.deleteFoodIdInCategory(foodId);
+//    }
+//
+//
+//    private void checkExistence(Category category, FoodId foodId) {
+//        category.findFoodId(foodId);
+//    }
+}
