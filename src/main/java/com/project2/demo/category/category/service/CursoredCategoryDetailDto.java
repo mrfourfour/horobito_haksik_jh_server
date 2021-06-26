17acbf2d2c366a55089c5de3fe456160c7221bb5
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-27 06:04:35 +0900

    fix: change class name

---
+++ src/main/java/com/project2/demo/category/category/service/CursoredCategoryDetailDto.java
@@ -0,0 +1,12 @@
+package com.project2.demo.category.category.service;
+
+import com.project2.demo.Menu.menu.service.MenuDto;
+import lombok.Value;
+
+import java.util.List;
+@Value
+public class CursoredCategoryDetailDto {
+    List<MenuDto> menuDtoList;
+    boolean hasNext;
+
+}
