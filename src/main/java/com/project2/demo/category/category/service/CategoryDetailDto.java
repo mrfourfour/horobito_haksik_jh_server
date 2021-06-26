a5c6050a364e9baf1282b7692ca798a7bd5b97fd
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-27 06:06:05 +0900

    fix: typo

--- src/main/java/com/project2/demo/category/category/service/CategoryDetailDto.java
+++ src/main/java/com/project2/demo/category/category/service/CategoryDetailDto.java
@@ -4,9 +4,11 @@ import com.project2.demo.Menu.menu.service.MenuDto;
 import lombok.Value;
 
 import java.util.List;
+
 @Value
 public class CategoryDetailDto {
+    Long categoryId;
+    String category;
+    String description;
     List<MenuDto> menuDtoList;
-    boolean hasNext;
-
 }
