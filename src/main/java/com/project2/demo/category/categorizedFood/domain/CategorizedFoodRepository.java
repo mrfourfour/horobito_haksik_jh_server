2f86761fa4840d5209d2ab9c2760d688e9ae7586
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-27 06:06:24 +0900

    feat: add methods

--- src/main/java/com/project2/demo/category/categorizedFood/domain/CategorizedFoodRepository.java
+++ src/main/java/com/project2/demo/category/categorizedFood/domain/CategorizedFoodRepository.java
@@ -1,7 +1,12 @@
 package com.project2.demo.category.categorizedFood.domain;
 
+import com.project2.demo.Menu.menu.service.MenuDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CategorizedFoodRepository extends JpaRepository<CategorizedFood, Long> {
     CategorizedFood findByCategoryIdAndMenuId(CategoryId categoryId, MenuId menuId);
+
+    List<CategorizedFood> findAllByCategoryId(CategoryId categoryId);
 }
