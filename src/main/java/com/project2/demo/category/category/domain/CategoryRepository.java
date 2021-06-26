e99bb14cb32379f1e8a5ef4f711866b2653c1721
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-27 06:05:16 +0900

    feat: create methods for <getAllDetails> method in [CategoryService]

--- src/main/java/com/project2/demo/category/category/domain/CategoryRepository.java
+++ src/main/java/com/project2/demo/category/category/domain/CategoryRepository.java
@@ -1,9 +1,16 @@
 package com.project2.demo.category.category.domain;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Category findCategoryById(Long categoryId);
+
+    List<Category> findAllByOrderByIdDesc(Pageable page);
+
+    List<Category> findByIdLessThanOrderByIdDesc(Long cursor, Pageable page);
 }
