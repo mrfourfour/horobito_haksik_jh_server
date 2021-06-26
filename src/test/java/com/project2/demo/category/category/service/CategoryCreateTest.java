7dbd520e06b2c9b54a97d637c86a75231fd13c9a
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-27 06:34:27 +0900

    test: add Mocks in [CategoryCreateTest] class

--- src/test/java/com/project2/demo/category/category/service/CategoryCreateTest.java
+++ src/test/java/com/project2/demo/category/category/service/CategoryCreateTest.java
@@ -1,11 +1,15 @@
 package com.project2.demo.category.category.service;
 
 import com.project2.demo.Menu.menu.domain.*;
+import com.project2.demo.Menu.menu.service.MenuService;
+import com.project2.demo.category.categorizedFood.domain.CategorizedFoodRepository;
+import com.project2.demo.category.category.domain.CategoryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -22,5 +26,14 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryCreateTest {
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @Mock
+    MenuRepository menuRepository;
+
+    @Mock
+    CategorizedFoodRepository categorizedFoodRepository;
 
 }
