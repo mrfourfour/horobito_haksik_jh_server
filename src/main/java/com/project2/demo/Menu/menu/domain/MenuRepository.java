12b861b8a95b303e1761f10503b98a3713aad417
jhkim <62026666+horobito@users.noreply.github.com>
2021-06-27 05:02:12 +0900

    feat: add <getDetailInfo> and other methods essential for this in [CategoryService] class

--- src/main/java/com/project2/demo/Menu/menu/domain/MenuRepository.java
+++ src/main/java/com/project2/demo/Menu/menu/domain/MenuRepository.java
@@ -1,6 +1,10 @@
 package com.project2.demo.Menu.menu.domain;
 
+import com.project2.demo.Menu.menu.service.MenuDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     Menu findMenuById(Long id);
@@ -8,4 +12,10 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
     Menu findMenuByIdAndDeleted(Long id, boolean deleted);
 
     Menu findMenuByTitle(Title title);
+
+    List<Menu> findAllByOrderByIdDesc(Pageable page);
+
+    List<Menu> findByIdLessThanOrderByIdDesc(Long cursor, Pageable page);
+
+    boolean existsByIdLessThan(Long cursor);
 }
--- src/main/java/com/project2/demo/category/category/service/CategoryService.java
+++ src/main/java/com/project2/demo/category/category/service/CategoryService.java
@@ -1,12 +1,19 @@
 package com.project2.demo.category.category.service;
 
+import com.project2.demo.Menu.menu.domain.Menu;
+import com.project2.demo.Menu.menu.service.MenuDto;
 import com.project2.demo.category.controller.CategoryParameter;
 import com.project2.demo.category.category.domain.*;
 import com.project2.demo.Menu.menu.domain.MenuRepository;
 import com.project2.demo.Menu.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -73,5 +80,38 @@ public class CategoryService {
 
     private Category getCategoryById(Long categoryId) {
         return categoryRepository.findCategoryById(categoryId);
+    }
+
+    public CategoryDetailDto getDetailInfo(Long categoryId, Long cursor, Pageable page) {
+        checkExistence(categoryId);
+        List<MenuDto> menuList = getMenus(cursor,  page)
+                .stream().map(this::getMenuDto).collect(Collectors.toList());
+        return new CategoryDetailDto(menuList, hasNext(cursor));
+
+
+    }
+
+    private boolean hasNext(Long cursor) {
+        if (cursor == null){
+            return false;
+        }
+        return menuRepository.existsByIdLessThan(cursor);
+    }
+
+    private MenuDto getMenuDto(Menu menu) {
+        return new MenuDto(
+                menu.getId(),
+                menu.getTitle(),
+                menu.getMenuDescription(),
+                menu.getImageURL(),
+                menu.getPrice(),
+                menu.getSalesTime().getStartTime(),
+                menu.getSalesTime().getEndTime()
+                );
+    }
+
+    private List<Menu> getMenus(Long cursor, Pageable page) {
+        return cursor == null ? menuRepository.findAllByOrderByIdDesc(page) :
+                menuRepository.findByIdLessThanOrderByIdDesc(cursor, page);
     }
 }
