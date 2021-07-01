c0aa87a25e378320ac99e1e022c4d2f0d8b16390
jhkim <62026666+horobito@users.noreply.github.com>
2021-07-01 21:40:15 +0900

    fix: add @Getter in [MenuId]

--- src/main/java/com/project2/demo/category/categorizedFood/domain/MenuId.java
+++ src/main/java/com/project2/demo/category/categorizedFood/domain/MenuId.java
@@ -1,11 +1,13 @@
 package com.project2.demo.category.categorizedFood.domain;
 
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Getter
 @NoArgsConstructor
 public class MenuId {
 
